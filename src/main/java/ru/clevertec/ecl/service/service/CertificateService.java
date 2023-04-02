c9be097826b4c210ded5c830dc8cebacd93e3b4d
Alexey Paramonov <paramonov_tama@mail.ru>
2023-04-02 22:44:18 +0300

    feat: @Transactional in CertificateService

--- src/main/java/ru/clevertec/ecl/service/service/CertificateService.java
+++ src/main/java/ru/clevertec/ecl/service/service/CertificateService.java
@@ -3,12 +3,14 @@ package ru.clevertec.ecl.service.service;
 
 import ru.clevertec.ecl.service.dto.GiftCertificateDto;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CertificateService {
 
 
     Long create(GiftCertificateDto giftCertificateDto);
-    boolean delete(Long id);
     Optional<GiftCertificateDto> findById(Long id);
+    List<GiftCertificateDto> findAll();
+    boolean delete(Long id);
 }
--- src/main/java/ru/clevertec/ecl/service/service/impl/CertificateServiceImpl.java
+++ src/main/java/ru/clevertec/ecl/service/service/impl/CertificateServiceImpl.java
@@ -3,10 +3,13 @@ package ru.clevertec.ecl.service.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.graph.GraphSemantic;
 import ru.clevertec.ecl.dal.dao.impl.GiftCertificateRepositoryHibernate;
+import ru.clevertec.ecl.dal.entity.GiftCertificate;
 import ru.clevertec.ecl.service.dto.GiftCertificateDto;
 import ru.clevertec.ecl.service.mapper.GiftCertificateMapper;
 import ru.clevertec.ecl.service.service.CertificateService;
 
+import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -16,17 +19,26 @@ public class CertificateServiceImpl implements CertificateService {
     private final GiftCertificateMapper giftCertificateMapper;
     private final GiftCertificateRepositoryHibernate giftRepository;
 
+    @Transactional
     @Override
-    public Optional<GiftCertificateDto> findById(Long id){
+    public Long create(GiftCertificateDto giftCertificateDto) {
+        GiftCertificate giftCertificate = giftCertificateMapper.toCertificate(giftCertificateDto);
+        return giftRepository.create(giftCertificate).getId();
+    }
+
+    @Transactional
+    @Override
+    public Optional<GiftCertificateDto> findById(Long id) {
         return giftRepository.findById(id)
                 .map(giftCertificateMapper::toCertificateDTO);
     }
 
     @Override
-    public Long create(GiftCertificateDto giftCertificateDto) {
-        return null;
+    public List<GiftCertificateDto> findAll() {
+        return giftRepository.findAll().stream().map(giftCertificateMapper::toCertificateDTO).toList();
     }
 
+    @Transactional
     @Override
     public boolean delete(Long id) {
         var maybeCertificate = giftRepository.findById(id);
